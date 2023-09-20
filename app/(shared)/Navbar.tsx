4a9306b8b02bf4a483b1b972458658f6cff87b43
Trynce Brandt <tjbrandt2013@gmail.com>
2023-09-20 19:27:57 -0400

    Navbar setup

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -1,24 +1,38 @@
-import Link from "next/link"
-import React from "react"
+import Link from "next/link";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
-    return (
-        <header className="mb-5">
-            <nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4">
-                <div className="flex justify-between items-center gap-10">
-                    <Link href="/">Home</Link>
-                    {/* <Link href="/">My Cocktails</Link> */}
-                    <Link href="/">About</Link>
-                </div>
-                <div>
-                    <p>Sign In</p>
-                </div>
-            </nav>
-            <hr className="border-1 mx-10" />
-        </header>
-    )
-}
+  return (
+    <header className="mb-5">
+      <nav className="flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4">
+        <div className="flex justify-between items-center gap-10 w-full">
+          {/* Logo */}
+          <div className="text-xl font-bold text-wh-10 flex-grow">
+            <Link href="/">
+              <div>PROHIBITION</div>
+            </Link>
+          </div>
 
-export default Navbar
+          {/* Navigation Links */}
+          <div className="flex gap-4">
+            <Link href="/">Home</Link>
+            <Link href="/my-cocktails">My Bar</Link>
+            <Link href="/about">About</Link>
+          </div>
+
+          {/* User Authentication */}
+          <div>
+            <Link href="/sign-in">
+              <div className="text-wh-10 hover:text-wh-50">Sign In</div>
+            </Link>
+          </div>
+        </div>
+      </nav>
+      <hr className="border-1 mx-10" />
+    </header>
+  );
+};
+
+export default Navbar;
